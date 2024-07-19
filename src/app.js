447f579d84d7fa16c947f77c5b10823c56697417
AguSSantos Prieto Atienza <131393036+agusSantos23@users.noreply.github.com>
2024-07-19 21:08:41 +0200

    0.4

--- src/app.js
+++ src/app.js
@@ -1,10 +1,12 @@
 import express from 'express'
 import authRoutes from './routes/auth.routes.js'
-
+import cors from 'cors'
 const app = express()
 
 
-
+app.use(cors({
+  origin: 'http://localhost:5173',
+}))
 app.use(express.json())
 
 app.use(authRoutes)
